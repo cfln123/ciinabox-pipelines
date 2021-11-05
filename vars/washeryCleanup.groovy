daf17e8e2e3c086d662c7dca9d85a5e9cef93907
Claudio Neto <claudio.ferreira.raizes@gmail.com>
2021-11-05 16:26:21 -0300

    Test

--- vars/washeryCleanup.groovy
+++ vars/washeryCleanup.groovy
@@ -5,18 +5,10 @@ washery DSL
 example usage
 washeryCleanup(
     region: 'ap-southeast-2', // (required)
-    accountId: '00000000000', // (optional, provide if assuming role in another account)
-    role: 'role-name', // (optional, provide if assuming role in another account)
-    sessionDuration: 3600 // (optional, extend the assume role session duration if need be. defaults to 3600)
-    snapshotId: 'snapshot-name', // (required, rds snapshot name or arn)
-    sqlScript: 'scrubber.sql', // (optional, anonymiser sql script to execute against the dataset)
-    scriptBucket: 's3-bucket-name', // (conditional, required if sqlScript is set)
-    instanceType: 'instance|cluster', // (required, cluster if using aurora, instance for mysql|postgres|sql-server rds)
-    instanceSize: 'db.t3.small', // (optional, overide the default instance sizes set by washery)
-    dumpBucket: 's3-bucket-name', // (optional, specify if dumping database to a s3 bucket)
-    saveSnapshot: true|false, // (optional, defaults to true. Determines if a snapshot is taken of the scrubbed database)
-    containerImage: 'ghcr.io/base2services/washery:v2', // (optional, the docker image to run in fargate, defaults to ghcr.io/base2services/washery:v2)
-    databases: ['mydb', 'anotherdb'] // (optional list of databases to dump, defaults to all databases)
+    awsAccountId: '00000000000', // (required)
+    prefix: 'washery-scrubbed', // (optional, snaphot name's prefix to filter)
+    keepVersions: 5, // (conditional, required if keepDays is not set, keep last N snapshots)
+    keepDays: 7 // (optional, required if keepVersions is not set, keep snapshots from last N days)
 )
 ************************************/
 
@@ -27,7 +19,10 @@ def call(body) {
   def response  = client.describeDBClusterSnapshotAttributes(request)
   def snapshots = response.getDBClusterSnapshots()
 
-
+  for (snapshot : snapshots) {
+    println snapshot.toString()
+    println snapshot.getDBClusterSnapshotArn()
+  }
 }
 
 // #!/usr/bin/env python3
