9ecf0b21e69d280eaf710f051e7e0b73acc64ce7
Claudio Neto <claudio.ferreira.raizes@gmail.com>
2021-11-09 17:14:26 -0300

    Testing dry run

--- vars/washeryCleanup.groovy
+++ vars/washeryCleanup.groovy
@@ -44,10 +44,9 @@ def _filter(s) {
 }
 def filterAndSortSnapshots(snapshots, prefix) {
   def filtered = snapshots
-    .toSorted { s1, s2 -> s1.getSnapshotCreateTime().compareTo(s2.getSnapshotCreateTime()) }
-    .collect
     .findAll { s -> s.getSnapshotType() == 'manual' && s.getDBClusterSnapshotIdentifier().startsWith(prefix) }
     .collect
+    .toSorted { s1, s2 -> s1.getSnapshotCreateTime().compareTo(s2.getSnapshotCreateTime()) }
   return filtered
     //
 }
