f4736714520b8aad36d333371015f0fae141a31b
Claudio Neto <claudio.ferreira.raizes@gmail.com>
2021-11-11 16:05:26 -0300

    Testing dry run

--- vars/washeryCleanup.groovy
+++ vars/washeryCleanup.groovy
@@ -48,7 +48,7 @@ def filterAndSortSnapshots(snapshots, identifier, tags) {
       for (t1 in tags) {
         def found = false
 
-        for (t2 in it.getTagList()) {
+        for (t2 in it.getTagList().values) {
           if (t1.key == t2.getName() && tag.getValue().matches(t2.value)) {
             found = true
             break
