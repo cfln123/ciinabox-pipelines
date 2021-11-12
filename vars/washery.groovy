b91e342f8deb0777eecfb7db37c7222f71ee7485
Claudio Neto <claudio.ferreira.raizes@gmail.com>
2021-11-12 16:20:51 -0300

    Custom washery tags

--- vars/washery.groovy
+++ vars/washery.groovy
@@ -75,8 +75,8 @@ def call(body) {
         for (tag in config.tags) {
             tags << [ Key: tag.key, Value: tag.value ]
         }
-        def tags_json = JsonOutput.toJson(tags).replace('"', '\\\\\"')
-        opts = "${opts} -t \\\'${tags_json}\\\'"
+        def tags_json = JsonOutput.toJson(tags).replace('"', "\\\"")
+        opts = """${opts} -t "${tags_json}" """
 
     }
         
