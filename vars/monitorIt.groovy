76981e626c28228c5a0fc9eb4c6556023a382c4f
Claudio Neto <claudio.ferreira.raizes@gmail.com>
2021-11-22 17:01:21 -0300

    Test

--- vars/monitorIt.groovy
+++ vars/monitorIt.groovy
@@ -3,13 +3,12 @@ def call(body) {
   def types     = config.get('serviceTypes', false)
   def duration  = config.get('sessionDuration', 900)
   def resources = ''
-  dir('/monitorable') {
+  
   withAWS(region: config.region, role: config.role, roleAccount: config.accountId, duration: duration, roleSessionName: 'monitorIt') {
-    
-    // sh(script: 'rm -rf monitorable && git clone https://github.com/base2Services/monitorable.git', label: 'monitorIt')
-    
-    //   sh(script: "python3 -m pip install -r requirements.txt", label: 'monitorIt')
-    resources = sh(script: "./monitorable.py --format cfn-guardian --regions $config.region", label: 'monitorIt')
+    sh(script: 'rm -rf monitorable && git clone https://github.com/base2Services/monitorable.git', label: 'monitorIt')
+    dir('./monitorable') {
+      sh(script: "sudo python3 -m pip install -r requirements.txt", label: 'monitorIt')
+      resources = sh(script: "./monitorable.py --format cfn-guardian --regions $config.region", label: 'monitorIt')
     }
   }
 
